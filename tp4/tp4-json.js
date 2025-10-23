792d3a9b1257f714459839cda969fbc416fccdbd
doura78 <sidibeabdouramane@hotmail.fr>
2025-10-23 22:16:19 +0200

    tp4

---
+++ tp4/tp4-json.js
@@ -0,0 +1,36 @@
+let societe = {
+    "nom" : "Google",
+    "siège social":{
+    "adresse" : "Googleplex",
+    "ville" : "Mountain View",
+    "etat" : "California", 
+    " pays" :  "United States"
+},
+
+ "Fondateurs" : [
+       {
+          "nom" : "Page",
+          "prenom" : "Larry",
+          "dateDeNaissance" : "26/03/1973",
+          "lieuDeNaissance" : "East Lansing",
+          "etatDeNaissance" : "Michigan",
+          "paysDeNaissance" : "Etaits-Unis"  
+       }, 
+       
+       {
+          "nom" : "Brin",
+          "prenom" : "Sergey",
+          "dateDeNaissance" : "21/08/1973",
+          "lieuDeNaissance" : "Moscou",
+          "paysDeNaissance" : "Union Soviétique" 
+       },
+       ],
+
+       "Chiffre d’affaires en milliards de $:" : [
+              {"année" : 2008, "montant" : 16.49},
+              {"année" : 2012, "montant" : 37.97},
+              {"année" : 2016, "montant" : 89.46},
+              {"année" : 2018, "montant" : 136.2}
+       ],  
+}
+
